91aa62f2cb1b6a8658c04e2c76470c6dc514f591
Joel Namuri <joelnamuri005@gmail.com>
2026-09-17 14:19:09 +1000

    fix: ESLint config for flat config format

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,3 @@
-import js from '@eslint/js';
-import tseslint from '@typescript-eslint/eslint-plugin';
 import tsparser from '@typescript-eslint/parser';
 
 export default [
@@ -9,9 +7,11 @@ export default [
       ecmaVersion: 2020,
       sourceType: 'module',
       parser: tsparser,
-    },
-    plugins: {
-      // eslint-plugin-next will be auto-loaded by eslint-config-next
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
     },
     rules: {
       // Rules from eslint-config-next will be applied
